02068fb709ccc04161b0b4bf61b7c6ba23cabfc8
paredesseba <paredessebastian013@gmail.com>
2020-03-24 16:30:47 -0300

    Parte C

---
+++ src/com/company/Profesor.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Profesor {
+
+    private String nombre;
+    private String apellido;
+    private Integer antiguedad;
+    private Integer codigoDeProfesor;
+
+    public Profesor(String nombre, String apellido, Integer antiguedad, Integer codigoDeProfesor) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.antiguedad = antiguedad;
+        this.codigoDeProfesor = codigoDeProfesor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public Integer getAntiguedad() {
+        return antiguedad;
+    }
+
+    public Integer getCodigoDeProfesor() {
+        return codigoDeProfesor;
+    }
+
+    public boolean equals (Object o){
+        if (this == o){
+            return true;
+        }
+        if (o == null){
+            return false;
+        }
+        if (!(o instanceof Profesor)){
+            return false;
+        }
+        Profesor profesorAComparar = (Profesor) o;
+        return  (profesorAComparar.getCodigoDeProfesor().equals(this.codigoDeProfesor));
+    }
+}
